84a1f2c1955ca7fe40fc36b766b46a82db09ccef
Ghadatounsi <ghadatounsi.2021@gmail.com>
2024-04-17 15:33:34 +0200

    commit

--- Frontend/include/navbar.php
+++ Frontend/include/navbar.php
@@ -10,18 +10,17 @@
     <div class="collapse navbar-collapse" id="navbarsFurni">
         <ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
             <li class="nav-item active">
-                <a class="nav-link" href="index.html">Accueil</a>
+                <a class="nav-link" href="http://localhost/projet1/Frontend/index.php">Accueil</a>
             </li>
-            <li><a class="nav-link" href="shop.html">Formations</a></li>
-            <li><a class="nav-link" href="about.html">À propos de nous</a></li>
-            <li><a class="nav-link" href="services.html">Services</a></li>
-            <li><a class="nav-link" href="blog.html">Actualité</a></li>
-            <li><a class="nav-link" href="contact.html">Contactez nous</a></li>
+            <li><a class="nav-link" href="http://localhost/projet1/Frontend/shop.php">Formations</a></li>
+            <li><a class="nav-link" href="http://localhost/projet1/Frontend/about.php">À propos de nous</a></li>
+            <li><a class="nav-link" href="http://localhost/projet1/Frontend/services.php">Services</a></li>
+            <li><a class="nav-link" href="http://localhost/projet1/Frontend/blog.php">Actualité</a></li>
+            <li><a class="nav-link" href="http://localhost/projet1/Frontend/contact.php">Contactez nous</a></li>
         </ul>
 
         <ul class="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
-            <li><a class="nav-link" href="#"><img src="images/user.svg"></a></li>
-            <li><a class="nav-link" href="cart.html"><img src="images/cart.svg"></a></li>
+            <li><a class="nav-link" href="http://localhost/projet1/Frontend/login.php"><img src="images/user.svg"></a></li>
         </ul>
     </div>
 </div>
