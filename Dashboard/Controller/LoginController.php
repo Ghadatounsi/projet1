3500b75a81ca04c675c453eb12e07287903225b1
Ghadatounsi <ghadatounsi.2021@gmail.com>
2024-04-26 17:26:16 +0200

    commit

--- Dashboard/Controller/LoginController.php
+++ Dashboard/Controller/LoginController.php
@@ -2,13 +2,15 @@
 // Inclure le modèle UserModel
 require_once('../Model/UserModel.php');
 
+// Démarrer la session
+session_start();
+
 // Créer une instance du modèle UserModel
 $userModel = new UserModel();
 
 // Vérifier si le formulaire de connexion a été soumis
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
     // Récupérer les données du formulaire
-    $id=$_POST['id'];
     $email = $_POST['email'];
     $password = $_POST['password'];
 
@@ -16,14 +18,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['login'])) {
     $user = $userModel->login($email, $password);
 
     if ($user) {
-        $id = $user['id'];
-        // L'utilisateur est connecté avec succès, vous pouvez rediriger vers une autre page par exemple
-        header("Location: http://localhost/projet1/dashboard/dashboard.php?id=$id");
+        // L'utilisateur est connecté avec succès, stocker l'ID de l'utilisateur dans la session
+        $_SESSION['user_id'] = $user['id'];
+        // Rediriger vers une autre page par exemple
+        header("Location: http://localhost/projet1/Dashboard/dashboard.php?id=" . $user['id']);
         exit(); // Terminer le script
     } else {
         // Afficher un message d'erreur par exemple
-        header('Location: http://localhost/projet1/dashboard/index.php');
-        $error = "Email ou mot de passe incorrect";
+        header("Location: http://localhost/projet1/Dashboard/index.php?error=1");
+        exit(); // Terminer le script
     }
 }
+
+
 ?>
